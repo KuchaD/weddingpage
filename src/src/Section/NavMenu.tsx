8c9e7addf417d8bb6174202464369be2f0fceec3
David kuchar <kuchar@dkuchar.cz>
2023-06-28 09:50:40 +0200

    fix: dev

---
+++ src/src/Section/NavMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const NavMenu: React.FC = () => {
+    return (
+        <div>
+            <ul>
+                <li>O nas</li>
+                <li>Naše cesta</li>
+                <li>My společne</li>
+                <li>Nahraj!</li>
+            </ul>
+        </div>
+        );
+}
